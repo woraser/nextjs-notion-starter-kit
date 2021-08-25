559c8dc3dfa187727fdb10a16cd81c29fbd092cd
charles.chen <809728954@qq.com>
2021-08-25 22:08:50 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ebaf35a6487f4c959232a22c23871710',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'WebSite',
+  domain: 'https://utf9.notion.site/WebSite-ebaf35a6487f4c959232a22c23871710',
+  author: 'Charles',
 
   // open graph metadata (optional)
   description: 'Example site description',
